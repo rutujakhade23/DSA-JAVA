d7757f6e20a0b620faf328ec5b29c375536196c5
Rutuja Khade <khaderutuja17@gmail.com>
2026-03-14 20:01:01 +0530

    Implement Linked List basics: Node class, addFirst and print methods

---
+++ LinkedList/LLBasics.java
@@ -0,0 +1,49 @@
+public class LLBasics {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public static void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+
+        addFirst(1);
+        addFirst(2);
+        addFirst(3);
+        addFirst(4);
+
+        print();
+
+    }
+}
